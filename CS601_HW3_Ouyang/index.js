3e2c98614935bed87b29cb1499ad3d7b99ebb247
Beining Ouyang <63318049+BeiningOuyang@users.noreply.github.com>
2022-05-25 15:58:09 -0400

    Assignment 3 in progress

--- CS601_HW3_Ouyang/index.js
+++ CS601_HW3_Ouyang/index.js
@@ -5,6 +5,8 @@ function get(id) {
 }
 
 let submit = get("submit");
+let tryAgain = get('try-again');
+tryAgain.style.visibility = "hidden";
 
 submit.onclick = function () {
   let username = get("name");
@@ -13,6 +15,24 @@ submit.onclick = function () {
   let sum = get("sum-number");
   let twoSum = Number(numberOne.value) + Number(numberTwo.value);
   sum.innerText = twoSum;
-
   console.log(twoSum);
+  if (twoSum > 10) {
+    let bigNum = get("big-small");
+    bigNum.innerText = "big number";
+    console.log("big number");
+  }
+  if (twoSum <= 10) {
+    let smallnum = get("big-small");
+    smallnum.innerText = " small number";
+    console.log("small number");
+  }
+  let calculator = get("calculator")
+  calculator.style.visibility = "hidden";
+  tryAgain.style.visibility = "visible";
 };
+
+
+tryAgain.onclick = function(){
+
+
+}

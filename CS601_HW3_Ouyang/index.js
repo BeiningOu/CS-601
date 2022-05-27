1dd67f705511b71cb783350935d8067b62f00e15
Beining Ouyang <63318049+BeiningOuyang@users.noreply.github.com>
2022-05-27 19:03:05 -0400

    adding prompt

--- CS601_HW3_Ouyang/index.js
+++ CS601_HW3_Ouyang/index.js
@@ -10,12 +10,34 @@ let submit = get("submit");
 let tryAgain = get("try-again");
 let stopGame = get("stop");
 let bye = get("bye");
-let gameOn = true;
+let alertWindowMode = true;
 
 tryAgain.style.visibility = "hidden";
 stopGame.style.visibility = "hidden";
 bye.style.display = "none";
 calculator.style.visibility = "hidden";
+
+let person = prompt("Please enter your name", "");
+
+if (person != null) {
+  welcome.innerHTML = "Hello " + person + "! How are you today?";
+  welcome.innerText =
+    username.value + ": welcome to Beining's simple JavaScript calculator";
+  alert(
+    "Hello!" +
+      person +
+      ": welcome to Beining's simple JavaScript calculator, let's do a sum of two numbers!"
+  );
+  let alertWindow = window.confirm(
+    "Do you want to do the calculation in alert windows? Click 'cancel' to enter the interface."
+  );
+  if (alertWindow) {
+    alertWindowMode = true;
+  } else {
+    alertWindowMode = false;
+  }
+  let nameBox = get("name-box");
+}
 
 submitName.onclick = function () {
   calculator.style.visibility = "visible";
@@ -23,48 +45,78 @@ submitName.onclick = function () {
     username.value + ": welcome to Beining's simple JavaScript calculator";
   let nameBox = get("name-box");
   nameBox.style.display = "none";
-};
+}
 
-submit.onclick = function () {
-  let username = get("name");
-  let numberOne = get("first-number");
-  let numberTwo = get("second-number");
-  let sum = get("sum-number");
-  let twoSum = Number(numberOne.value) + Number(numberTwo.value);
-  sum.innerText =
-    "The sum of " +
-    numberOne.value +
-    " and " +
-    numberTwo.value +
-    " is " +
-    twoSum;
-  console.log(twoSum);
-  if (twoSum > 10) {
-    let bigNum = get("big-small");
-    bigNum.innerText = "This is a big number.";
-    console.log("big number");
-  }
-  if (twoSum <= 10) {
-    let smallNum = get("big-small");
-    smallNum.innerText = " This is a small number.";
-    console.log("small number");
+  while (alertWindowMode) {
+    let firstNumber = prompt("Please type your first number");
+    let secondNumber = prompt("Please type your second number");
+
+    // need to check whether it is a number.//
+    let sumOfTwo = Number(firstNumber.value) + Number(secondNumber.value);
+
+    window.confirm(sumOfTwo.value);
+
+    if (sumOfTwo > 10) {
+      let bigNumber = get("big-small");
+      alert("This is a big number.");
+    }
+    if (sumOfTwo <= 10) {
+      alert("This is a big number.");
+    }
+
+    let answer = window.confirm("Do one more time?");
+
+    alertWindowMode = false;
   }
 
-  calculator.style.display = "none";
-  tryAgain.style.visibility = "visible";
-  stopGame.style.visibility = "visible";
-};
+  submitName.onclick = function () {
+    calculator.style.visibility = "visible";
+    welcome.innerText =
+      username.value + ": welcome to Beining's simple JavaScript calculator";
+    let nameBox = get("name-box");
+    nameBox.style.display = "none";
+  };
 
-tryAgain.onclick = function () {
-  calculator.style.display = "flex";
-  tryAgain.style.visibility = "hidden";
-  stopGame.style.visibility = "hidden";
-  let gameOn = true;
-};
+  submit.onclick = function () {
+    let username = get("name");
+    let numberOne = get("first-number");
+    let numberTwo = get("second-number");
+    let sum = get("sum-number");
+    let twoSum = Number(numberOne.value) + Number(numberTwo.value);
+    sum.innerText =
+      "The sum of " +
+      numberOne.value +
+      " and " +
+      numberTwo.value +
+      " is " +
+      twoSum;
+    console.log(twoSum);
+    if (twoSum > 10) {
+      let bigNum = get("big-small");
+      bigNum.innerText = "This is a big number.";
+      console.log("big number");
+    }
+    if (twoSum <= 10) {
+      let smallNum = get("big-small");
+      smallNum.innerText = " This is a small number.";
+      console.log("small number");
+    }
 
-stopGame.onclick = function () {
-  let mainBox = get("main-box");
-  mainBox.style.display = "none";
-  bye.style.display = "block";
-  let gameOn = false;
-};
+    calculator.style.display = "none";
+    tryAgain.style.visibility = "visible";
+    stopGame.style.visibility = "visible";
+  };
+
+  tryAgain.onclick = function () {
+    calculator.style.display = "flex";
+    tryAgain.style.visibility = "hidden";
+    stopGame.style.visibility = "hidden";
+    let gameOn = true;
+  };
+
+  stopGame.onclick = function () {
+    let mainBox = get("main-box");
+    mainBox.style.display = "none";
+    bye.style.display = "block";
+    let gameOn = false;
+  };

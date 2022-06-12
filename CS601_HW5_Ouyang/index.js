5e0d5964aa3be2b961bc8ef44c53a506e3e136df
Beining Ouyang <63318049+BeiningOuyang@users.noreply.github.com>
2022-06-11 23:37:57 -0400

    hw5 css

--- CS601_HW5_Ouyang/index.js
+++ CS601_HW5_Ouyang/index.js
@@ -1,6 +1,10 @@
+function get(id) {
+  return document.getElementById(id);
+}
+
 // get the JSON file from local using "fetch"
-document.addEventListener("click", () => {
-  let url = "academic.json";
+let btn_local = get("click-here");
+btn_local.addEventListener("click", () => {
   fetch("academic.json")
     .then((response) => {
       console.log("response.headers: " + response.headers);
@@ -8,32 +12,47 @@ document.addEventListener("click", () => {
       console.log("response.statusText: " + response.statusText);
       return response.json();
     })
-    .then((data) => console.log(data))
+    .then((data) => {
+      console.log(data);
+      return renderHTMLlocal(data, get("fetch-locally"));
+    })
+    .then(() => (get("click-here").style.display = "none"))
     .catch((e) => console.log("Oops, error"));
 });
 
-let btn = document.getElementById("fetch-button");
-let demo = document.getElementById("demo");
-
+// get the JSON file externally using AJAX
+let btn = get("fetch-button");
 btn.addEventListener("click", () => {
   let ourRequest = new XMLHttpRequest();
   ourRequest.open(
     "get",
     "https://learnwebcode.github.io/json-example/animals-1.json"
   );
-
   ourRequest.onload = () => {
     let ourData = JSON.parse(ourRequest.responseText);
-    renderHTML(ourData);
+    console.log(ourData);
+    renderHTMLext(ourData, get("fetch-externally"));
   };
-
   ourRequest.send();
+  btn.style.display = "none";
 });
 
-function renderHTML(data) {
+function renderHTMLlocal(data, HTMLtag) {
   let htmlString = "";
   for (i = 0; i < data.length; i++) {
-    htmlString += "<p>" + data[i].name + " is a " + data[i].species + ".</p>";
+    htmlString += `<p> School name: ${data[i].schoolName} </p>
+    <p> Program/Major: ${data[i].programMajor} </p>
+    <p> Degree type: ${data[i].degreeType} </p>
+    <p> Year conferred: ${data[i].yearConferred} </p>
+    <br />`;
   }
-  demo.insertAdjacentHTML("beforeend", htmlString);
+  HTMLtag.insertAdjacentHTML("beforeend", htmlString);
+}
+
+function renderHTMLext(data, HTMLtag) {
+  let htmlString = "";
+  for (i = 0; i < data.length; i++) {
+    htmlString += `<p> ${data[i].name} is a ${data[i].species} </p>`;
+  }
+  HTMLtag.insertAdjacentHTML("beforeend", htmlString);
 }

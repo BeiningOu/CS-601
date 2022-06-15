ce2595bfa600667626ec1e130cf84962c20c6c9c
Beining Ouyang <63318049+BeiningOuyang@users.noreply.github.com>
2022-06-15 14:29:06 -0400

    v-if

--- CS601_HW6_Ouyang/index.js
+++ CS601_HW6_Ouyang/index.js
@@ -3,17 +3,27 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      msg: "Hello Vue!",
-      pictureBigcat: "assets/bigcat.jpg",
+      msg: "",
+      pictureTrans: "assets/bigcat.jpg",
+      kitten: true,
     };
   },
 
   methods: {
     mouseover() {
-      this.pictureBigcat = "assets/kitten.png";
+      this.pictureTrans = "assets/kitten.png";
     },
     mouseleave() {
-      this.pictureBigcat = "assets/bigcat.jpg";
+      this.pictureTrans = "assets/bigcat.jpg";
+    },
+
+    changePic() {
+      this.kitten = !this.kitten;
+      if (this.kitten == false) {
+        this.msg = "show big cat";
+      } else {
+        this.msg = "hide big cat";
+      }
     },
   },
 }).mount("#app");

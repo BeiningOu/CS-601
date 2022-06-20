a3dbf95b600d4fbaf3a5925f5f61b15020a7eaab
Beining Ouyang <63318049+BeiningOuyang@users.noreply.github.com>
2022-06-20 11:54:02 -0400

    add back to top

--- CS601_TermProject_Ouyang/main.js
+++ CS601_TermProject_Ouyang/main.js
@@ -2,6 +2,19 @@ function get(id) {
   return document.getElementById(id);
 }
 
+let backToTop = get("back-top");
+
+backToTop.onclick = function () {
+  console.log("back");
+  let timer = setInterval(function () {
+    if (document.documentElement.scrollTop != 0) {
+      document.documentElement.scrollTop -= 50;
+    } else {
+      clearInterval(timer); // stop the setInterval..
+    }
+  }, 10);
+};
+
 let menuLabel = get("menu-label");
 let navBar = get("nav-bar");
 let readMore = get("read-more-main");

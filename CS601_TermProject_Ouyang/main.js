7b3cc13c0025ef2bf27ecc6cfd2768866a005d2f
Beining Ouyang <63318049+BeiningOuyang@users.noreply.github.com>
2022-06-19 23:02:32 -0400

    main page-1

--- CS601_TermProject_Ouyang/main.js
+++ CS601_TermProject_Ouyang/main.js
@@ -4,8 +4,11 @@ function get(id) {
 
 let menuLabel = get("menu-label");
 let navBar = get("nav-bar");
+let readMore = get("read-more-main");
+let readMoreHide = get("read-more-hide");
 
 navBar.style.display = "none";
+readMoreHide.style.display = "none";
 
 menuLabel.onclick = function () {
   if (navBar.style.display == "block") {
@@ -15,52 +18,6 @@ menuLabel.onclick = function () {
   }
 };
 
-//   submitName.onclick = function () {
-//     calculator.style.visibility = "visible";
-//     welcome.innerText =
-//       username.value + ": welcome to Beining's simple JavaScript calculator";
-//     let nameBox = get("name-box");
-//     nameBox.style.display = "none";
-//   };
-
-//   submitNum.onclick = function () {
-//     let username = get("name");
-//     let numberOne = get("first-number");
-//     let numberTwo = get("second-number");
-//     let sum = get("sum-number");
-//     let twoSum = Number(numberOne.value) + Number(numberTwo.value);
-//     sum.innerText =
-//       "The sum of " +
-//       numberOne.value +
-//       " and " +
-//       numberTwo.value +
-//       " is " +
-//       twoSum;
-//     console.log(twoSum);
-//     if (twoSum > 10) {
-//       let bigNum = get("big-small");
-//       bigNum.innerText = "This is a big number.";
-//       console.log("big number");
-//     }
-//     if (twoSum <= 10) {
-//       let smallNum = get("big-small");
-//       smallNum.innerText = " This is a small number.";
-//       console.log("small number");
-//     }
-
-//     calculator.style.display = "none";
-//     tryAgain.style.visibility = "visible";
-//     stopGame.style.visibility = "visible";
-//   };
-
-//   tryAgain.onclick = function () {
-//     calculator.style.display = "flex";
-//     tryAgain.style.visibility = "hidden";
-//     stopGame.style.visibility = "hidden";
-//   };
-
-//   stopGame.onclick = function () {
-//     let mainBox = get("main-box");
-//     mainBox.style.display = "none";
-//     bye.style.display = "block";
-//   };
+readMore.onclick = function () {
+  readMoreHide.style.display = "block";
+};
